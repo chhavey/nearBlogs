06be2565082ab5fbc96d4549db33ad673f425185
chhavey <guptachhavi880@gmail.com>
2024-10-10 20:52:00 +0530

    database connected, models created

---
+++ server/models/Blog.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    region: { type: String, required: true },
+    published: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Blog", blogSchema);
